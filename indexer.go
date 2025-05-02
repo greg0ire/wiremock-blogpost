c448a3fbac322e20d14c42042942e46b6e2f94c5
Grégoire Paris <gregoire.paris@manomano.com>
2025-05-02 14:34:08 +0200

    Add Wiremock in record mode

--- indexer.go
+++ indexer.go
@@ -1,25 +1,15 @@
 package indexer
 
 import (
-	"os"
-
 	"github.com/algolia/algoliasearch-client-go/v4/algolia/search"
 )
 
-func indexRecord() { // nolint:unused // we do not want to write a full-fleged app
-	appID := os.Getenv("ALGOLIA_APP_ID")
-	apiKey := os.Getenv("ALGOLIA_API_KEY")
+func indexRecord(client *search.APIClient) { // nolint:unused // we do not want to write a full-fleged app
 	indexName := "test-index"
 
 	record := map[string]any{
 		"objectID": "object-1",
 		"name":     "test record",
-	}
-
-	client, err := search.NewClient(appID, apiKey)
-
-	if err != nil {
-		panic(err)
 	}
 
 	// Add record to an index
